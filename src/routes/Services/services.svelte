5fcc4711b4f11b590543744c29d86635506cd5f8
MiguelPlasencia1902 <miguelplasencia1902@gmail.com>
2024-05-20 19:04:42 -0500

    Services completed

---
+++ src/routes/Services/services.svelte
@@ -0,0 +1,98 @@
+
+<div class="section-services">
+    <h1 class="title-services">SERVICES</h1>
+    <hr class="separator">
+    <p class="text-title">Our multidisciplinary engineering services</p>
+</div>
+<div class="text-services">
+    <div class="text1">
+        Ciavolella engineering provides highly qualified services in <span style="color: #555555"><b>Design & Engineering
+            Project Management, Construction Management</b></span> and we make them even better througth <span style="color: #555555; text-decoration: underline"><b>BIM</b></span> (Building information modeling) methodology.
+    </div>
+    <div class="text2">
+        "Commited to innovattion and excellent in all we do"
+    </div>
+    <div class="text3">
+        Our main Sectors:
+        <ol type="A">
+            <li>Pharmaceutical</li>
+            <li>Life sciencie, and healthcare</li>
+            <li>Chemical</li>
+            <li>Industrial</li>
+        </ol>
+    </div>
+</div>
+
+
+
+<style>
+
+    @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Jersey+25&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Sora:wght@100..800&display=swap');
+
+    .section-services{
+        margin-top: 3rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .title-services{
+        color: #AAAAAA;
+        font-size: 3rem;
+        font-family: "Sora", sans-serif;
+    }
+
+    .separator{
+        width: 5%;
+        border: 5px solid #1B99E8;
+        margin: 1rem;
+    }
+
+    .text-title{
+        font-size: 1.5rem;
+        padding-bottom: 3rem;
+        font-family: "Sora", sans-serif;
+    }
+
+    .text-services{
+        background-color: #54B3EE;
+        width: 100%;
+        justify-content: center;
+    }
+
+    .text1{
+        font-family: "Sora", sans-serif;
+        color: #FFFFFF;
+        margin: 25%;
+        margin-top: 0;
+        margin-bottom: 0;
+        padding-top: 5rem;
+        justify-content: center;
+        font-size: 1.5rem;
+    }
+
+    .text2{
+        font-family: "Sora", sans-serif;
+        color: #FFFFFF;
+        margin: 25%;
+        margin-top: 0;
+        margin-bottom: 0;
+        justify-content: center;
+        font-size: 1.5rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        font-style: italic;
+    }
+
+    .text3{
+        font-family: "Sora", sans-serif;
+        color: #FFFFFF;
+        margin: 25%;
+        margin-top: 0;
+        margin-bottom: 0;
+        justify-content: center;
+        font-size: 1.5rem;
+        padding-bottom: 5rem;
+    }
+    
+</style>
